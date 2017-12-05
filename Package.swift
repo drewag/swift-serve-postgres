a8f9f34bb0c1438b722af066be9a82a15c32f171
Andrew J Wagner <drewag@gmail.com>
2017-12-05 13:50:30 -0700

    [IC] Update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-serve-postgres",
+    name: "SwiftServePostgres",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
